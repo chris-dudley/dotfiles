30e0f496b429c1b538300f38a439d8aeb62241bd
Christopher Dudley <chris@terainsights.com>
2015-12-14 18:04:40 -0500

    Add rpm-spec layer. Add spotify, themes.

--- .spacemacs
+++ .spacemacs
@@ -13,7 +13,7 @@ values."
    dotspacemacs-distribution 'spacemacs
    ;; List of additional paths where to look for configuration layers.
    ;; Paths must have a trailing slash (i.e. `~/.mycontribs/')
-   dotspacemacs-configuration-layer-path '()
+   dotspacemacs-configuration-layer-path '("~/git/dotfiles/spacemacs-layers")
    ;; List of configuration layers to load. If it is the symbol `all' instead
    ;; of a list then all discovered layers will be installed.
    dotspacemacs-configuration-layers
@@ -37,7 +37,11 @@ values."
      syntax-checking
      scala
      javascript
+     rpm-spec
+     ;;php
      ;; version-control
+     spotify
+     themes-megapack
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
---
+++ spacemacs-layers/rpm-spec/extensions.el
@@ -0,0 +1,31 @@
+;;; extensions.el --- rpm-spec Layer extensions File for Spacemacs
+;;
+;; Copyright (c) 2012-2014 Sylvain Benner
+;; Copyright (c) 2014-2015 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(setq rpm-spec-pre-extensions
+      '(
+        ;; pre extension names go here
+        ))
+
+(setq rpm-spec-post-extensions
+      '(
+        ;; post extension names go here
+        ))
+
+;; For each extension, define a function rpm-spec/init-<extension-name>
+;;
+;; (defun rpm-spec/init-my-extension ()
+;;   "Initialize my extension"
+;;   )
+;;
+;; Often the body of an initialize function uses `use-package'
+;; For more info on `use-package', see readme:
+;; https://github.com/jwiegley/use-package
